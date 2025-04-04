f643b67a1d43f62585fb2074dd67adb7ef9ae7b2
Braden <bradenchinjiashen@gmail.com>
2025-04-04 17:26:02 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,1 +1,19 @@
-print("this is the interface")
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("YOU'RE in trouble")
+
+#to keep this constantly running (controlling the main game loop)
+game_running = True
+while game_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_running = False #stop the loop
+
+pygame.quit
+
