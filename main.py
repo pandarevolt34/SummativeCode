3914f7e59195dc0be6a7f85e47f4edd783ff13ed
pandarevolt34 <rayan.abdallah@warwick.ac.uk>
2025-04-09 23:35:40 +0100

    removed fenwick tree from code.

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 #CS Python Project
 
 import random
-# ID: 5674312
-class FenwickTree:  # the stack of cards template
-    def __init__(self):  # initializing class
-        self.tree = []  # will store the stack so that we can operate adding and removing cards in log(n) time
-        self.powers_of_2 = [0]*7301
-        self.number_of_cards = 72
-
-    def initiate_powers_of_2(self):  # store powers of 2 in a list
-        for i in range(26):
-            self.powers_of_2.append(2 ** i)  # powers_of_2[index] = 2 ^ index
-
-    def add_card_position_to_tree(self, card):  # Adds card to the tree structure
-        index = card.index # take the index of the card in the array
-        while index <= 7300:
-            self.tree[index] += 1 # add presence of the number
-            index += index & (-index) # set largest OFF binary bit to ON
-
-    def remove_top_card_from_tree(self):
-        # to be continued
-        return
 
 # ID: 5676233
 ''' Class for Cards:
@@ -214,3 +194,5 @@ class RedBlackTree:
             else:
                 print("BLACK")
             node = node.right
+# to be continued
+# ID 5674312
